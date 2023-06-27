4f75f3b83a174a777df96692d849f0f9fe86815d
Fernando Suarez <Fernando.pala.99@gmail.com>
2023-06-27 18:40:50 -0500

    LIsto quedo full

--- Manuela.js
+++ Manuela.js
@@ -1,6 +1,11 @@
+const operacion = require("./Relacion.js");
+
+operacion.Felicidad ("Si");
+
 function Sueño(){
 
     console.log("Manuela duerme muchisimo ");
 
     console.log("No, enserio; Ella Duerme demasiado ")
 }
+
--- Relacion.js
+++ Relacion.js
@@ -1,4 +1,9 @@
 function Felicidad( Ayuda){
 
+ if(Ayuda = "Si"){
+    console.log ("Super genial");
+ }
 
 }
+
+module.exports  = { "Felicidad": Felicidad}
